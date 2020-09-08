c9db30f7be17a9c85eca37d145d76f66e8e285fb
ikp-773 <abhijithkp773@gmail.com>
2020-09-09 00:21:00 +0530

    Update annualReport.js

--- js/annualReport.js
+++ js/annualReport.js
@@ -1,3 +1,13 @@
-$('.flip').on('click', function() {
-  ('.pannel').slideToggle('slow');
-});
+var totalReports= document.querySelectorAll('.flip').length;
+
+for (var i = 0; i < totalReports; i++) {
+  document.querySelectorAll(".flip")[i].addEventListener('click', handleClick);
+}
+
+handleClick(){
+  if (this.style.display === "none") {
+   this.style.display = "block";
+ } else {
+   this.style.display = "none";
+ }
+}
